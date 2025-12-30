80de7eeb857c87141462caf57a47ed05de9ef8de
Mahalakshmi-2023 <venkatamahalakshmigogineni@gmail.com>
2025-12-30 15:53:21 +0530

    Add feature to view tasks

--- todo.py
+++ todo.py
@@ -1,5 +1,10 @@
 def main():
     print("Welcome to To-Do List Manager")
-
+def view_tasks(tasks):
+    if not tasks:
+        print("No tasks available")
+    for i, task in enumerate(tasks):
+        status = "Done" if task["done"] else "Pending"
+        print(f"{i+1}. {task['task']} [{status}]")
 if __name__ == "__main__":
     main()
